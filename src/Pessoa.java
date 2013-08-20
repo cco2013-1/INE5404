9a4e0ce522b59dd4f02f55f7f000ffa73415b4f3
Matheus Ben-Hur <mtbmatheus@hotmail.com>
2013-08-20 11:45:37 -0300

    Inicio Projeto, Classe Pessoa

---
+++ src/Pessoa.java
@@ -0,0 +1,82 @@
+public class Pessoa {
+
+	private String nome;
+	private int cpf;
+	private int telefoneMovel;
+	private int telefoneFixo;
+	private String tratamento;
+	private String nascimento;
+	private String email;
+	private String profissional;
+
+	public Pessoa (String vNome, int vCPF, int vTelefoneMovel,int vTelefoneFixo, String vTratamento, String vNascimento,String vEmail, String vProfissional) {
+		nome = vNome;
+		cpf=vCPF;
+		telefoneMovel=vTelefoneMovel;
+		telefoneFixo=vTelefoneFixo;
+		tratamento=vTratamento;
+		nascimento = vNascimento;
+		email=vEmail;
+		profissional=vProfissional;
+	}
+	public String toString(){
+		String mensagem = "Nome: "+this.nome+
+				"\n"+"CPF: "+this.cpf+
+				"\n"+"Data de Nascimento: "+this.nascimento+
+				"\n"+"Telefone Celular: "+this.telefoneMovel+" Telefone Fixo: "+this.telefoneFixo+
+				"\n"+"E-mail: "+this.email+
+				"\n"+"Tratamento: "+this.tratamento+
+				"\n"+"Profissional: "+this.profissional;
+		return mensagem;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public int getTelefoneMovel() {
+		return telefoneMovel;
+	}
+	public void setTelefoneMovel(int telefoneMovel) {
+		this.telefoneMovel = telefoneMovel;
+	}
+	public int getTelefoneFixo() {
+		return telefoneFixo;
+	}
+	public void setTelefoneFixo(int telefoneFixo) {
+		this.telefoneFixo = telefoneFixo;
+	}
+	public String getTratamento() {
+		return tratamento;
+	}
+	public void setTratamento(String tratamento) {
+		this.tratamento = tratamento;
+	}
+	public String getNascimento() {
+		return nascimento;
+	}
+	public void setNascimento(String nascimento) {
+		this.nascimento = nascimento;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getProfissional() {
+		return profissional;
+	}
+	public void setProfissional(String profissional) {
+		this.profissional = profissional;
+	}
+	
+
+}
