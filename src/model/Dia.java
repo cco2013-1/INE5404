93c2f97f3ae5aa72ac06fcc2e13284d4171388e9
Vinicius Teixeira <vinicius0026@gmail.com>
2013-08-28 16:04:12 -0300

    Criados os métodos get(int i), get(Hora h) e numHorarios, na classe Dia.java

--- src/model/Dia.java
+++ src/model/Dia.java
@@ -2,6 +2,7 @@ package model;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
@@ -14,11 +15,22 @@ import java.util.List;
  */
 
 public class Dia {
-
-	//Atributos
-	private ArrayList<Horario> horarios; //Lista dos horários
 	
+	//Atributos
+	private List<Horario> horarios;
+	
+	/**
+	 * Construtor Dia
+	 * Cria objeto Dia, com horários a partir de horarioInicio,
+	 * cada horario com a duracao especificada, até o horarioFinal.
+	 * Deve ser definida a Sala para qual este dia se refere.
+	 * @param horarioInicio Horário de início de atendimento em dada sala
+	 * @param horarioFinal Horário final de atendimento
+	 * @param duracao Duração de cada intervalo
+	 * @param sala Sala para o qual os horários se referem
+	 */
 	public Dia(Hora horarioInicio, Hora horarioFinal, int duracao, Sala sala){
+		
 		horarios = new ArrayList<Horario>();
 		
 		Hora inicioAula = new Hora(horarioInicio);
@@ -36,8 +48,41 @@ public class Dia {
 		}
 	}
 	
-	public ArrayList<Horario> getHorarios(){
-		return this.horarios;
+	public Horario get(int i){
+		return this.horarios.get(i);
+	}
+	
+	/**
+	 * Método get(hora)
+	 * Retorna Horário de forma que a hora fornecida no construtor é maior ou igual
+	 * à hora de início do horário retornado.
+	 * Ex. Fornecida hora: 15h30.
+	 * Horários contidos na lista de horários do dia: (14h00 - 14h50) (14h50 - 15h40) (15h40 - 16h30)
+	 * Será retornado o segundo horário na lista, ou seja, (14h50 - 15h40) pois 15h30 >= 14h50 
+	 * @param h
+	 * @return
+	 */
+	public Horario get(Hora h){
+		Horario hor = null;
+		
+		for ( int i = 0; i < this.horarios.size(); i++){
+			if ( h.getTime().compareTo(this.get(i).getInicio().getTime() ) >= 0) {
+				hor = this.get(i);
+				break;
+			}
+		}
+		
+		return hor;
+	}
+	
+	/**
+	 * Método numHorarios
+	 * Retorna o número de horários criados no
+	 * objeto Dia
+	 * @return
+	 */
+	public int numHorarios(){
+		return this.horarios.size();
 	}
 	
 	public static void main(String[] args){
@@ -48,11 +93,9 @@ public class Dia {
 		
 		Dia d = new Dia(inicio, fim, 50, s);
 		
-		ArrayList<Horario> h = d.getHorarios();
-		
-		for(int i = 0; i < h.size(); i++){
-			Hora comeco = h.get(i).getInicio();
-			Hora fin = h.get(i).getFim();
+		for(int i = 0; i < d.numHorarios(); i++){
+			Hora comeco = d.get(i).getInicio();
+			Hora fin = d.get(i).getFim();
 			System.out.println("Inicio: " + comeco );
 			System.out.println("Fim: " + fin );
 			System.out.println("\n");
