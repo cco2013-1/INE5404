af5386c5b2a7cad1989a7c0296f399c899052066
Vinicius Teixeira <vinicius0026@gmail.com>
2013-08-28 16:04:54 -0300

    Criada classe SQLDatabase.java que implementa a interface Database. Esta classe ainda não foi implementada!

---
+++ src/model/SQLDatabase.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.List;
+import java.util.Map;
+
+public class SQLDatabase implements Database {
+
+	@Override
+	public int gravar(String tabela, Map<String, Object> dados) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Map<String, List<Object>> consultar(List<String> colunas,
+			List<String> tabelas, Map<String, Object> parametros) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean atualizar(String tabela, String coluna, Object valor,
+			String colunaCriterio, Object criterio) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
