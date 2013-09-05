6d39943c78561ba458e3d503000f7226a0d9654f
Vinicius Teixeira <vinicius0026@gmail.com>
2013-09-05 12:35:57 -0300

    Teste Git - classe Especialidade

--- src/model/negocio/Especialidade.java
+++ src/model/negocio/Especialidade.java
@@ -1,8 +1,15 @@
 package model.negocio;
 
 public class Especialidade {
-
+	
+	//Constantes
+	private static final String TABELA = "especialidades";
+	
+	
+	//Atributos
 	private List<Tratamento> tratamento;
+	
+	
 	public Especialidade(List<Tratamento>) {
 		
 	}
